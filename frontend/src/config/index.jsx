9445cdf119b9ef3c1784a1e202002a146c84e914
ShivamPatel82 <shiv821107@gmail.com>
2026-06-22 01:15:19 +0530

    Update backend URL

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const { default: axios } = require("axios");
 
-export const BASE_URL = "http://localhost:3030"
+export const BASE_URL = "https://linkedin-clone-61go.onrender.com"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
