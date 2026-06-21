b14fc9dfa02dc388cfcfab45999d2d0eb6eb5a36
ShivamPatel82 <shiv821107@gmail.com>
2026-06-22 00:54:08 +0530

    initDB

--- backend/config/intiDB.js
+++ backend/config/intiDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 async function connToDB(){
    try{
-    await mongoose.connect("mongodb+srv://shiv821107:Shivam1020@linkedinclone.5yf3w.mongodb.net/");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to DB");
    }catch(err){
     console.log("Error in db connection", err);
